7f843fd16283bac3860170238b2c805125a0548d
Luis Ramirez <niosin_amor@hotmail.com>
2026-03-03 00:24:30 -0600

    estoy creando mi proyecto diversos datos de JS

---
+++ README.md
@@ -0,0 +1,14 @@
+# Mi Proyecto
+
+## Captura de pantalla
+
+![Vista del proyecto](img/navegador.png)
+
+# Estoy inicializando mi proyecto.
+![Vista del proyecto](img/navegador.png)
+
+# En este punto agrego imagen para demostrar como mando a llamar a las variables.
+![Vista del proyecto](img/console.png)
+
+# Aqui sigo agregando más elementos para mi entregable
+![Vista del proyecto](img/agregandoMasConst.png)
---
+++ main.js
@@ -0,0 +1,21 @@
+//alert("hi")
+console.log("my name is luis, como estas")
+console.error("Hola mundo como estan")//muestra si hay algún error
+console.warn("precaution")
+console.table("BMW", "MCLaren", "Ferrari", "Audi", "Mercedes Bens")
+
+const carros = ["BMW", "MCLAREN", "FERRARI", "AUDI", "MERCEDES BENZ"]
+console.log("----tipos de datos----", carros, typeof carros)
+console.log(carros [4], typeof carros)
+console.log(carros [2], typeof carros [2])
+console.log("Mi carro favorito es el: ", carros [1], typeof carros [1])
+console.log(`Mi segundo carro favorito es el: ${carros [2]}`)
+
+//Boolean
+
+const buenDesarrollador = true
+console.log("buenDesarrollador", typeof buenDesarrollador)
+console.log(`Luis sera un excelente Desarrollador ${buenDesarrollador}`)
+
+const calificacion = null
+console.log("mi calificacion sera", calificacion, "ya que dependera de mi proyecto", typeof calificacion)
